dad55d67a4bdda6672c365767254fdb05bcf7299
Yousef Hosny <yousefhosny.hm@gmail.com>
2022-10-13 16:45:44 +0200

    fixed html to show encounters in console

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
+document.querySelector('.click').addEventListener('click', getFetch)
+
 function getFetch(){
     const choice = document.querySelector('input').value
     
